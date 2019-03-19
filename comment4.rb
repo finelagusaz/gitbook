e78da71e24ceb89ad53a08e83a39e1c5f8c62cf1
Fine Lagusaz <algiz.rune@gmail.com>
2019-03-19 12:19:10 +0900

    チャプター 2-4まで

---
+++ comment4.rb
@@ -0,0 +1,11 @@
+puts 1
+=begin
+puts 2
+puts 3
+puts 4
+=end
+puts 5
+
+=begin
+#複数行コメントの中に1行コメントを入れても大丈夫っぽい
+=end
---
+++ const.rb
@@ -0,0 +1,6 @@
+#定数
+#   大文字から始まる名前では定数になる
+CaffeLatte = "カフェラテ"
+puts CaffeLatte
+CaffeLatte = "カフェラッテ"
+puts CaffeLatte
